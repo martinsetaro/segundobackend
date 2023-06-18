0bf2f318940138d8a912b3398ce72cbdd9ffc8a2
martin setaro <lordsesony@gmail.com>
2023-06-18 03:37:06 -0300

    dldkd

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import datosUsuarios from '../app/routes/datos.js';
 import servidor from '../app/routes/servidor.js';
 
 const app = express();
@@ -10,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/servidor', servidor);
-app.use('/usuario', datosUsuarios);
+
 
 app.listen(port, () => {
   console.log(`El servidor está funcionando en el puerto ${port}`);
