beee991b5eb78fb4be1e9652ff6dba8067acca64
martin setaro <lordsesony@gmail.com>
2023-06-18 04:21:15 -0300

    cambios nuevos

--- app/routes/servidor.js
+++ app/routes/servidor.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Desde el servidor");
+  res.send("Desde el servidor esta funcionando correctamente");
 });
 
 export default router;
